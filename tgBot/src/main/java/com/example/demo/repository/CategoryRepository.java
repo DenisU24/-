5af4a7466a29a9f0c8f27e4e59b640d3b6677d38
Denis Umanets <denisumanec36@gmail.com>
2024-07-03 23:28:26 +0300

    Практическое задание 2. Реализована страница Hello wotrld
    Практическое задание 3. Разработаны сущности и протестирован ввод данных
    
    Изменен путь в ClientOrderRepository
    
    Практическое задание 3
    
    Исправлен порядок объявления полей и методов. В связах установлен параметр optional.

---
+++ tgBot/src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "categories", path = "categories")
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ tgBot/src/main/java/com/example/demo/repository/ClientOrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.ClientOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "clientOrders", path = "clientOrders")
+public interface ClientOrderRepository extends JpaRepository<ClientOrder, Long> {
+}
